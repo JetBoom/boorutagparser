39a8a3eb0d849d42e9d4591aeb967cc5ea56b70f
William Moodhe <williammoodhe@gmail.com>
2021-07-28 14:32:07 -0400

    Version bump

--- boorutagparser.user.js
+++ boorutagparser.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Booru Tag Parser
 // @namespace    http://average.website
-// @version      1.1.5
+// @version      1.1.6
 // @description  Copy current post tags and rating on boorus and illustration2vec in to the clipboard for easy import in to a program or another booru.
 // @author       William Moodhe
 // @downloadURL  https://github.com/JetBoom/boorutagparser/raw/master/boorutagparser.user.js
